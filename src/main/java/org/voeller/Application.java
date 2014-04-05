8fc8cc82aeedc0313671e0be4499f0db1a0d9a28
Sean Voeller <sean.voeller@gmail.com>
2014-04-05 13:56:02 -0700

    Initial commit

---
+++ src/main/java/org/voeller/Application.java
@@ -0,0 +1,3 @@
+package org.voeller;
+
+public interface Application {}
---
+++ src/main/java/org/voeller/config/DataSourceConfig.java
@@ -0,0 +1,9 @@
+package org.voeller.config;
+
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+
+@Configuration
+public interface DataSourceConfig {
+    DataSource dataSource();
+}
